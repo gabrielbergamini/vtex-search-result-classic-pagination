95248d90113c121aa0961a63b7d2ae32471f32e7
Gabriel Bergamini <gabrielbergamini.dev@gmail.com>
2023-03-24 16:18:29 -0300

    CSS fix and formatting

--- react/Pagination.js
+++ react/Pagination.js
@@ -80,13 +80,18 @@ const Pagination = () => {
   return (
     <div
       style={{ marginTop: "40px" }}
-      className={`${handles.pagination} flex flex-wrap justify-center`}
+      className={`${handles.pagination} flex flex-wrap justify-center flex-column items-center`}
     >
       <div
         className={`${handles.paginationinfo} flex justify-center items-center ma2 flex-wrap bg-black-80 ph5 pv3 br4 f6 white`}
         // style={{ display: "flex", width: "100%", justifyContent: "center" }}
       >
-        <svg className={`w1-ns mr3`} style={{fill: "white"}} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+        <svg
+          className={`w1-ns mr3`}
+          style={{ fill: "white" }}
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 512 512"
+        >
           <path d="M40 48C26.7 48 16 58.7 16 72v48c0 13.3 10.7 24 24 24H88c13.3 0 24-10.7 24-24V72c0-13.3-10.7-24-24-24H40zM192 64c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H192zm0 160c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H192zm0 160c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H192zM16 232v48c0 13.3 10.7 24 24 24H88c13.3 0 24-10.7 24-24V232c0-13.3-10.7-24-24-24H40c-13.3 0-24 10.7-24 24zM40 368c-13.3 0-24 10.7-24 24v48c0 13.3 10.7 24 24 24H88c13.3 0 24-10.7 24-24V392c0-13.3-10.7-24-24-24H40z" />
         </svg>
 
@@ -101,7 +106,7 @@ const Pagination = () => {
             <>
               <span
                 key={thePage}
-                className={`${handles.paginationbuttonactive} inline-block bg-black-20 br2 pa5 ma2 black self-start`}
+                className={`${handles.paginationbuttonactive} inline-block bg-black-20 br2 pa4 ma2 black self-start`}
               >
                 {thePage}
               </span>
@@ -110,7 +115,7 @@ const Pagination = () => {
             <>
               <a
                 key={thePage}
-                className={`${handles.paginationbutton} no-underline inline-block bg-black-10 br2 pa5 ma2 near-black self-start hover-bg-black-20`}
+                className={`${handles.paginationbutton} no-underline inline-block bg-black-10 br2 pa4 ma2 near-black self-start hover-bg-black-20`}
                 href={`/${queryData.query}?page=${
                   thePage !== "Primeira" &&
                   thePage !== "Última" &&
